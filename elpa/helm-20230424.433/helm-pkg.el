2ae0cf8b03ac46cb9e51b6c529d8427d959e428b
William Neveu <williamneveu@gmail.com>
2023-06-24 09:24:55 -0400

    1st commit, xps

---
+++ elpa/helm-20230424.433/helm-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm" "20230424.433" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.0")
+    (popup "0.5.3"))
+  :commit "75e21fb81bcd24c0d42c8cfddea37237418a1650" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
